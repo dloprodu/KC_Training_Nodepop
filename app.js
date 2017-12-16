9ce7dae24f5eff806ae45c7059be8a0b0f9e926e
David López Rguez <dloprodu@gmail.com>
2017-12-16 22:22:26 +0000

    ESLint added

--- app.js
+++ app.js
@@ -4,9 +4,10 @@
  * @version 1.0.0
  */
 
+'use strict';
+
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -30,17 +31,21 @@ require('./lib/mongooseConnection');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+// creamos nuestro engine html para 
+// poder user la extensión .html en las vistas
+app.engine('html', require('ejs').__express);
 
 // log only 4xx and 5xx responses to console
 app.use(logger('dev', {
-  skip: function (req, res) { return res.statusCode < 400; }
+  skip: (req, res) => {
+    return res.statusCode < 400; 
+  }
 }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+// support encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -67,14 +72,14 @@ app.use('/api/v1/:lang(en|es)/users', language, users);
 app.use('/api/v1/:lang(en|es)/ads',   language, ads);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   const customError = new NodepopError(err, req.language, isAPI(req));
 
   res.status(customError.status || 500);
